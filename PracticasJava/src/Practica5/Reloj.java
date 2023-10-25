e962bb25442a13d8c5bfda95a373aa0f44feb43c
BereniceLC <148828589+BereniceLC@users.noreply.github.com>
2023-10-25 10:58:48 -0600

    Nueva clase

---
+++ PracticasJava/src/Practica5/Reloj.java
@@ -0,0 +1,16 @@
+package Practica5;
+import java.util.*;
+
+public class Reloj extends TareaPeriodica {
+	
+	public Reloj(){
+		super(60);
+	}
+	
+	public String leerHora() {
+		Calendar cal = new GregorianCalendar();
+		return cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE);
+	}
+	
+	
+}
