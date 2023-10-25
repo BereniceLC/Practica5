753f6ba932eb9aec2328a592f9c4dabb63155984
BereniceLC <148828589+BereniceLC@users.noreply.github.com>
2023-10-25 16:01:57 -0600

    Modificacion del metodo main

--- PracticasJava/src/Practica5/Controladora.java
+++ PracticasJava/src/Practica5/Controladora.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 public class Controladora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//modificacion del metodo main
+		new Thread(new Reloj()).start();
+		new Thread(new Ejecutor("calc",0)).start();
+		
+		/*TODO Auto-generated method stub
 		Ejecutor e = new Ejecutor("calc", 1);
 		Scanner leer = new Scanner(System.in);
 		System.out.println("Escribe un comando para ejecutar");			
 		e.comando = leer.next();
 		if (e.necesitaEjecucion()) {
 			e.ejecutarTarea();
-		}
+		}*/
 	}
 }
