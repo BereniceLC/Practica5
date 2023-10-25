5f4ec8fc1771e92b3760c0c2a6747cd9af45e6f7
BereniceLC <148828589+BereniceLC@users.noreply.github.com>
2023-10-25 11:00:49 -0600

    Nueva clase

---
+++ PracticasJava/src/Practica5/Ejecutor.java
@@ -0,0 +1,16 @@
+package Practica5;
+import java.util.*;
+import java.io.IOException;
+
+public class Ejecutor extends TareaPeriodica{
+	
+	private String comando;
+	
+	public Ejecutor(String comando, int periodo){
+		super(periodo);
+		this.comando = comando;
+	}
+	
+	public String leerComando(){ return comando; }
+		
+}
